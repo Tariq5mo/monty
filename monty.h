501fc810cfd8d5ceed88ba9db5d9b36cfbbe4ec6
Talal4024 <Talal4024@gmail.com>
2024-03-30 05:42:39 +0000

    Fix

--- monty.h
+++ monty.h
@@ -22,9 +22,9 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
